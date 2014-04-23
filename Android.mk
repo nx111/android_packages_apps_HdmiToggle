3d1a6edc4a1cc4da9ef6a8331363ea7d65d3053a
nx111 <gd.zhangdz@gmail.com>
2014-04-23 15:58:09 +0800

    rename package name to HdmiToggle
    Change-Id: Icd12a8d95c35fc65810c7afa519302f2edbf6b6c

--- Android.mk
+++ Android.mk
@@ -7,8 +7,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := HDMI_Toggle
-LOCAL_CERTIFICATE := testkey
+LOCAL_PACKAGE_NAME := HdmiToggle
 
 include $(BUILD_PACKAGE)
 
